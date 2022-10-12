290b39cb3a4ffa88a5bcf2842c59c30729c94be9
whitekf <90643199+whitekf@users.noreply.github.com>
2022-10-12 14:26:29 -0400

    Updated code based on MOOOKS pinned comment recommendation provided

--- skipYouTubeAds.py
+++ skipYouTubeAds.py
@@ -1,24 +1,28 @@
 import pyautogui
-# pyautogui.FAILSAFE = True
+import time
+
 
 def findAndClick():
     while True:
-        videoAdCords = pyautogui.locateCenterOnScreen("play.png")
-        bannerCords = pyautogui.locateCenterOnScreen("bannerad.png")
-        blackBannerCords = pyautogui.locateCenterOnScreen("black banner.png")
+        videoAdCords = pyautogui.locateCenterOnScreen("play.png", confidence=0.9, region=(1208, 0, 1585, 1079))
+        bannerCords = pyautogui.locateCenterOnScreen("bannerad.png", confidence=0.9, region=(931, 0, 1100, 1079))
+        blackBannerCords = pyautogui.locateCenterOnScreen("black banner.png", confidence=0.9, region=(931, 0, 1100, 1079))
+        print(videoAdCords)
+        print(bannerCords)
+        print(blackBannerCords)
 
         if videoAdCords or bannerCords or blackBannerCords is not None:
             break
 
-        if videoAdCords is not None:
-            pyautogui.click(videoAdCords)
-        elif bannerCords is not None:
-            pyautogui.click(bannerCords)
-        if blackBannerCords is not None:
-            pyautogui.click(blackBannerCords)
+    if videoAdCords is not None:
+        pyautogui.click(videoAdCords)
+    elif bannerCords is not None:
+        pyautogui.click(bannerCords)
+    elif blackBannerCords is not None:
+        pyautogui.click(blackBannerCords)
 
-        findAndClick()
+    time.sleep(1)
+    findAndClick()
+
 
 findAndClick()
-
-#print(f'Skipping all of the YouTube ads')
